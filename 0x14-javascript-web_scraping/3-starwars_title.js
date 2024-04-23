0c6c37295befaddadf1551292c4763d4658bd9d8
ThokozaniLegend <teejey2@gmail.com>
2024-04-23 04:23:36 -0700

    fix errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let id = process.argv[2];
-let url = 'http://swapi.co/api/films/' + id;
+let url = 'https://swapi-api.alx-tools.com/api/films/' + id ;
 const request = require('request');
 
 request(url, function (err, response, body) {
@@ -13,4 +13,4 @@ request(url, function (err, response, body) {
   } else {
     console.log('Erorr Code:' + response.statusCode);
   }
-});
+})
